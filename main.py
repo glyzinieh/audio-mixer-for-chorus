fb6dded9fef4073df20ea357d344639c5d5f0487
glyzinieh <wisteriatp@gmail.com>
2022-03-28 22:48:42 +0900

    window表示部を作成

--- main.py
+++ main.py
@@ -1,28 +1,35 @@
 import tkinter as tk
 from tkinter import filedialog, ttk
 
-
 #pageを遷移
 def change_page(page):
     #pageを最前にする
     page.tkraise()
 
-def select_mix(root_frame):
-    select_mix_page = ttk.Frame(root_frame)
-
-def file_select(root_frame):
-    file_select_page = ttk.Frame(root_frame)
-
 def main():
-    #windowを作成
+    #windowを生成
     root=tk.Tk()
     root.title("合唱練習音源作成用ミックスツール")
     #root.geometry("x")
-    #file_selecteを読み込み
-    file_select(root)
-    #select_mixを読み込み
-    select_mix(root)
+    #ウィンドウのグリッドを1x1にする
+    root.grid_rowconfigure(0,weight=1)
+    root.grid_columnconfigure(0,weight=1)
+    
+    #----------File Select----------
+    file_select_page = ttk.Frame(root)
+    button = {}
+    for i in range(4):
+        button[i] = ttk.Button(file_select_page,text="send",command=lambda:change_page(select_mix_page))
+        button[i].pack()
+    file_select_page.grid(row=0,column=0,sticky="nsew")
+
+    #----------Select Mix----------
+    select_mix_page = ttk.Frame(root)
+    radio = ttk.Radiobutton(select_mix_page).pack()
+    select_mix_page.grid(row=0,column=0,sticky="nsew")
+
     #実行
+    file_select_page.tkraise()
     root.mainloop()
 
 if __name__ == "__main__":
